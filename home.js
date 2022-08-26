fd5c3b086075f75899a7b5d47131c2a5a1efc8c2
kerbeysaurus <112154359+kerbeysaurus@users.noreply.github.com>
2022-08-26 14:08:48 +0800

    hmtl .java

---
+++ home.js
@@ -0,0 +1,67 @@
+
+//Numbers in Javascript
+//var num1=5.3;
+//console.log(6*6);
+
+/* Function
+1. Create a function
+2. Call the function
+
+//Create
+function = fun() {
+    console.log('this is function');
+}
+
+//Call
+fun();
+
+/* Let's create a function that take in a name and returns says What's up followed by your name
+
+For example
+
+Name: "Kerberos"
+Return: "What's up Kerberos"
+*/
+/*
+function greeting(yourName) {
+   
+    var result = ('Whats up ') + yourName ;
+    console.log(result);
+
+}
+var name = prompt('Who is u?');
+greeting(name);
+
+
+function sumNumbers(a, b){
+    var result = a + b;
+    console.log(a + b);
+}
+sumNumbers(90, 65);
+*/
+/* WHile LOops
+
+var num = 0;
+
+while (num < 100 ) {
+    num += 1;
+    console.log(num);
+}
+
+*/
+
+/* loop
+
+for(var num = 0; num < 100; num++); {
+    console.log(num);
+}
+ 
+*/
+
+// DAta types
+let softdrinks = 'coke';
+let moreSoftdrinks = 'coke\sprite\royal\cocacola'; // new line
+
+console.log(softdrinks.length);
+console.log(softdrinks.indexOf('o'));
+console.log(softdrinks.slice(1, 4));
